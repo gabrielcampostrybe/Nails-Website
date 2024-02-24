e50e4705cc5d2381dfcaef8916b9a6ef72a26246
gabrielcampostrybe <gabrielcampos78@gmail.com>
2024-02-24 15:51:58 -0300

    add starter navbar and starter examples page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Examples from './pages/Examples';
 import HomePage from './pages/HomePage';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/examples' element={<Examples />} />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './HomePage.css';
+import Navbar from '../components/Navbar';
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a basic home page.</p>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<h1 className='title'>Welcome to the Home Page</h1>
+		</div>
+	);
 };
 
 export default HomePage;

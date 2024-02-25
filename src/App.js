abed8577a11d85b9cbe4991f8e14a86480691a44
gabrielcampostrybe <gabrielcampos78@gmail.com>
2024-02-25 17:48:32 -0300

    add products template description and css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import AboutMe from './mainPages/AboutMe';
 import Gallery from './mainPages/Gallery';
 import HomePage from './mainPages/HomePage';
 import Products from './mainPages/Products';
+import BaseCover from './productsPages/BaseCover';
 import ConstructorGel from './productsPages/ConstructorGel';
+import Fiber from './productsPages/Fiber';
+import NailDrill from './productsPages/NailDrill';
+import NailPolish from './productsPages/NailPolish';
+import Step1And2 from './productsPages/Step1And2';
+import TopCoat from './productsPages/TopCoat';
 
 const App = () => {
 	return (
@@ -16,13 +22,13 @@ const App = () => {
 				<Route path='/aboutme' element={<AboutMe />} />
 				<Route path='/products' element={<Products />} />
 				<Route path='/products/constructor-gel' element={<ConstructorGel />} />
-				<Route path='/products/base-cover' element={<ConstructorGel />} />
-				<Route path='/products/top-coat' element={<ConstructorGel />} />
-				<Route path='/products/nail-polish' element={<ConstructorGel />} />
-				<Route path='/products/fiber' element={<ConstructorGel />} />
-				<Route path='/products/nail-drill' element={<ConstructorGel />} />
-				<Route path='/products/step-1-and-2' element={<ConstructorGel />} />
-				</Routes>
+				<Route path='/products/base-cover' element={<BaseCover />} />
+				<Route path='/products/top-coat' element={<TopCoat />} />
+				<Route path='/products/nail-polish' element={<NailPolish />} />
+				<Route path='/products/fiber' element={<Fiber />} />
+				<Route path='/products/nail-drill' element={<NailDrill />} />
+				<Route path='/products/step-1-and-2' element={<Step1And2 />} />
+			</Routes>
 		</Router>
 	);
 };
--- src/productsPages/Step1And2.js
+++ src/productsPages/Step1And2.js
@@ -8,6 +8,59 @@ const Step1And2 = () => {
 			<Navbar />
 			<h3>Passo 1 e Passo 2</h3>
 			<img src={passo1e2} alt='imagem do passo 1 e passo 2' />
+      <h4>Descrição do produto</h4>
+			<div className='constructor-gel-desc'>
+				<br />
+				<p>
+					Gel para unhas com formulação inovadora. Possui Tecnologia
+					CurvaturaSmart que permite Estrutura Naturalidade com alta resistência
+					e durabilidade. Desenvolvido dentro dos nossos laboratórios, o Gel
+					Construtor Querido Nude Bluwe foi pensado minuciosamente e performa
+					incrivelmente em qualquer tipo de unha natural.
+				</p>
+				<p>
+					Os Géis Construtores Bluwe são tudo o que há de mais moderno do
+					mercado de matéria prima. Fizemos uma formulação com Tecnologia Smart,
+					ou seja, conseguimos uma curvatura de memória, onde não precisa o gel
+					ser rígido para segurar a curvatura. Com isso, os alongamentos não
+					ficam duros e quebradiços, e sim flexíveis e com memória de formato.
+					Com ele é mais seguro para alcançar a tão almejada Naturalidade.
+				</p>
+				<p>Viscosidade ideal para ser auto nivelante sem escorrer.</p>
+				<p>
+					Pode ser usado em todas as etapas: capa base para unhas normais,
+					extensão e ponto de tensão.
+				</p>
+				<p>
+					<b>O encanto:</b> Viscosidade ideal para manuseio facilitado, nivela
+					mas não escorre com facilidade.Formulação incrível para naturalidade.
+					Baixo aquecimento.
+				</p>
+				<p>
+					<b>COR:</b> Querido Nude - Nude intenso de cobertura média com subtom
+					quente.
+				</p>
+				<p>
+					<b>MODO DE USO:</b> Aplique camadas do produto sobre a unha, moldando
+					da forma desejada e coloque para catalisar sob luz uv por 120
+					segundos.
+				</p>
+				<p>
+					<b>INDICAÇÃO DE USO:</b> O Gel Construtor Querido Nude Bluwe pode ser
+					usado em todas as etapas do alongamento, da capa base ao ponto de
+					tensão.
+				</p>
+				<p>
+					<b>COMPOSIÇÃO:</b> Acrylates Copolymer, Trimethylolpropane
+					Trimethacrylate, Triethylene Glycol Dimethacrylate, Isobornyl
+					Methacrylate, Trimethylbenzoyl Diphenylphosphine Oxide, Benzoyl
+					Isopropanol, Poly Methyl Styrene Isopropanol Ketone,
+					Trimethylbenzophenone, Methyl Benzophenone, Methacryloylethyl
+					Phosphate, Benzoyl Isopropanol, BHA, Calcium Aluminum Borosilicate.
+					Pode conter os corantes: CI 15850, CI 60725, CI 77891, CI 77491, CI
+					77019.
+				</p>
+			</div>
 		</>
 	);
 };
